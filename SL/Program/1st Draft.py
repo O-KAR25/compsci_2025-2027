eed1554b56c321054b12745eceaade428a3ec1ed
O-KAR25 <oskarkrasowski8@gmail.com>
2025-11-18 16:33:36 +0100

    Misclick

---
+++ SL/Program/1st Draft.py
@@ -0,0 +1,13 @@
+import time
+import cachetools 
+import requests
+
+def posts():
+    letime = 
+    PostSend = False
+    Wanted = input()
+    while True:
+        if time == Wanted:
+            PostSend = True
+            break
+    if PostSend == True:
