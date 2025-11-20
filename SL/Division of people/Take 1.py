f5bb7e24f0728b70bc1745ebb122f4690293ef12
O-KAR25 <oskarkrasowski8@gmail.com>
2025-11-20 10:33:33 +0100

    Fixed the division of people algorythm

--- SL/Division of people/Take 1.py
+++ SL/Division of people/Take 1.py
@@ -2,25 +2,69 @@ import time
 
 
 
-def TheSortingHat(person,interestwol,interestssub1,interestssub2,interestssub3):
-
+def TheSortingHat():
    
     Scifi = ["math", "compsci"]
     Human = ["geo", "buisness", "tok", "advisory", "english", "psychology"]
     HumanChoice = 0
     ScifiChoice = 0 
-    if interestssub1 in Scifi:
-        ScifiChoice += 1
-    elif interestssub1 in Human:
-        HumanChoice += 1
-    if interestssub2 in Scifi:
-        ScifiChoice += 1
-    elif interestssub2 in Human:
-        HumanChoice += 1
-    if interestssub3 in Scifi:
-        ScifiChoice += 1
-    elif interestssub3 in Human:
-        HumanChoice += 1
+    House = "Not accepted, get outha hea"
+
+    print("Please input your name")
+    person = input()
+    print("Input whether you like Visual or Kinesthetic learning")
+    interestwol = input()
+    while True:
+        if interestwol == "visual" or "kinesthetic":
+            break
+        else:
+            print("Choose either visual or kinesthetic")
+            interestwol = input()
+
+    print("Your favourite 1 subject (subjects to choose from geo, buisness, tok, advisory, english, psychology, math and compsci )")
+    interestssub1 = input()
+    while True: 
+        if interestssub1 in Scifi:
+            ScifiChoice += 1
+            break
+        elif interestssub1 in Human:
+            HumanChoice += 1
+            break
+        else:
+            print("choose an actual subject that you can choose")
+            interestssub1 = input()
+            
+
+    print("Your favourite second subject (subjects to choose from geo, buisness, tok, advisory, english, psychology, math and compsci )")
+    interestssub2 = input()
+    while True:
+        if interestssub2 in Scifi:
+            ScifiChoice += 1
+            break
+
+        elif interestssub2 in Human:
+            HumanChoice += 1
+            break
+
+        else:
+            print("choose an actual subject that you can choose")
+            interestssub2 = input()
+
+    print("Your favourite 3rd subject (subjects to choose from geo, buisness, tok, advisory, english, psychology, math and compsci")
+    interestssub3 = input()
+    while True:
+
+        if interestssub3 in Scifi:
+            ScifiChoice += 1
+            break
+
+        elif interestssub3 in Human:
+            HumanChoice += 1
+            break 
+        else:
+            print("choose an actual subject that you can choose")
+            interestssub3 = input()
+
     if HumanChoice > ScifiChoice and interestwol == "visual":
         House = "Gryffindor"
     elif HumanChoice > ScifiChoice and interestwol == "kinesthetic":
@@ -37,21 +81,34 @@ def TheSortingHat(person,interestwol,interestssub1,interestssub2,interestssub3):
     print(f"I see you like {interestssub1}, {interestssub2} and {interestssub3}")
     print(f"You also like to learn using {interestwol}")
     print("Let me think who you should be now")
-    time.sleep(10)
-    
     print("")
+    time.sleep(5)
+    print("")    
+    time.sleep(0.1)
     print("")
+    time.sleep(0.1)
     print("")
+    time.sleep(0.1)
     print("")
+    time.sleep(0.1)
     print("")
+    time.sleep(0.1)
     print("")
+    time.sleep(0.1)
     print("")
+    time.sleep(0.1)
     print("")
+    time.sleep(0.1)
     print("")
+    time.sleep(0.1)
     print("")
+    time.sleep(0.1)
     print("")
+    time.sleep(0.1)
     print("")
-    print("")
+    time.sleep(0.1)
+    print("intense music starts playing")
+    time.sleep(2)
     print( House + "!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 
-TheSortingHat(input("Please input your name").strip, input("Input whether you like Visual or Kinesthetic learning").strip, input("Your favourite 1 subject (subjects to choose from geo, buisness, tok, advisory, english, psychology, math and compsci )").strip, input("Your favourite second subject (subjects to choose from geo, buisness, tok, advisory, english, psychology, math and compsci )").strip, input("Your favourite 1 subject (subjects to choose from geo, buisness, tok, advisory, english, psychology, math and compsci").strip)
+TheSortingHat()
